4bb4ba25435482614a950b2e55563c8cd5151dc8
demigodk <bfkalombo@gmail.com>
2020-03-17 15:26:10 +0200

    Initial project setup, with default Identity functionality

---
+++ tshilobo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using tshilobo.Data;
+
+[assembly: HostingStartup(typeof(tshilobo.Areas.Identity.IdentityHostingStartup))]
+namespace tshilobo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
